ea57db8bce0bd4fdb68a076de800c8d806080c44
WeeCameron04 <clochran04@gmail.com>
2024-01-30 23:41:25 +0000

    about me page started

--- src/components/AboutMePage/AboutMePage.js
+++ src/components/AboutMePage/AboutMePage.js
@@ -1,15 +1,28 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 import "./AboutMePage.css";
 
 const AboutMePage = () => {
   return (
     <>
-      <div className="header">
-        <header>
+      <div>
+        <header className="about-me-header">
           <center>
             <h1>About Me</h1>
           </center>
         </header>
+        <br />
+        <br />
+        <br />
+        <div className="about-me-info">
+          <center>
+            <Fade left>
+              <div className="fact-one">
+                <p> I Love Dinosaurs</p>
+              </div>
+            </Fade>
+          </center>
+        </div>
       </div>
     </>
   );
